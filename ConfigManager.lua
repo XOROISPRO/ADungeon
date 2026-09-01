b62076edddeeb8344495d419c50b2d3bb82e0c1c
XOROISPRO <xoro4690@gmail.com>
2026-09-01 21:33:27 +0800

    Update ConfigManager.lua

--- ConfigManager.lua
+++ ConfigManager.lua
@@ -29,38 +29,58 @@ function ConfigManager:GetPath(configName: string): string
 end
 
 function ConfigManager:ExportSettings(): { [string]: any }
-	return {
-		Toggles = self.Toggles or {},
-		Options = self.Options or {},
+	local exportData = {
+		Toggles = {},
+		Options = {}
 	}
+
+	for key, toggleObj in pairs(self.Toggles) do
+		if type(toggleObj) == "table" and toggleObj.Value ~= nil then
+			exportData.Toggles[key] = toggleObj.Value
+		end
+	end
+
+	for key, optionObj in pairs(self.Options) do
+		if type(optionObj) == "table" and optionObj.Value ~= nil then
+			exportData.Options[key] = optionObj.Value
+		end
+	end
+
+	return exportData
 end
 
 function ConfigManager:ImportSettings(data: { [string]: any })
 	if data.Toggles then
 		for key, val in pairs(data.Toggles) do
-			self.Toggles[key] = val
+			if self.Toggles[key] and type(self.Toggles[key].SetValue) == "function" then
+				self.Toggles[key]:SetValue(val)
+			end
 		end
 	end
 
 	if data.Options then
 		for key, val in pairs(data.Options) do
-			self.Options[key] = val
+			if self.Options[key] and type(self.Options[key].SetValue) == "function" then
+				self.Options[key]:SetValue(val)
+			end
 		end
 	end
 
+	-- Trigger module parameter updates directly
 	if self.Modules.PathfindingModule then
-		if self.Options.HoverHeight then self.Modules.PathfindingModule:SetHoverHeight(self.Options.HoverHeight) end
-		if self.Options.WalkSpeed then self.Modules.PathfindingModule:SetWalkSpeed(self.Options.WalkSpeed) end
-		if self.Options.AirVelocitySlider then self.Modules.PathfindingModule:SetAirVelocity(self.Options.AirVelocitySlider) end
-		if self.Options.AirAccelSlider then self.Modules.PathfindingModule:SetAirAccel(self.Options.AirAccelSlider) end
+		if data.Options and data.Options.HoverDistanceSlider then self.Modules.PathfindingModule:SetHoverHeight(data.Options.HoverDistanceSlider) end
+		if data.Options and data.Options.FlySpeedSlider then self.Modules.PathfindingModule:SetWalkSpeed(data.Options.FlySpeedSlider) end
+		if data.Options and data.Options.AirVelocitySlider then self.Modules.PathfindingModule:SetAirVelocity(data.Options.AirVelocitySlider) end
+		if data.Options and data.Options.AirAccelSlider then self.Modules.PathfindingModule:SetAirAccel(data.Options.AirAccelSlider) end
+		if data.Options and data.Options.OffsetDistanceSlider then self.Modules.PathfindingModule:SetOffsetDistance(data.Options.OffsetDistanceSlider) end
 	end
 
-	if self.Modules.AbilityModule and self.Options.AbilityMode then
-		self.Modules.AbilityModule:SetMode(self.Options.AbilityMode)
+	if self.Modules.AbilityModule and data.Options and data.Options.AbilityModeDropdown then
+		self.Modules.AbilityModule:SetMode(data.Options.AbilityModeDropdown)
 	end
 
-	if self.Modules.AutoClickerModule and self.Options.ClickInterval then
-		self.Modules.AutoClickerModule:SetInterval(self.Options.ClickInterval)
+	if self.Modules.AutoClickerModule and data.Options and data.Options.ClickIntervalSlider then
+		self.Modules.AutoClickerModule:SetInterval(data.Options.ClickIntervalSlider)
 	end
 end
 
@@ -77,7 +97,7 @@ function ConfigManager:SaveConfig(configName: string): boolean
 
 	if success and encoded then
 		writefile(path, encoded)
-		print("[ConfigManager] Saved config: " .. configName)
+		print("[ConfigManager] Successfully saved config: " .. configName)
 		return true
 	end
 
@@ -98,7 +118,7 @@ function ConfigManager:LoadConfig(configName: string): boolean
 
 	if success and type(decoded) == "table" then
 		self:ImportSettings(decoded)
-		print("[ConfigManager] Loaded config: " .. configName)
+		print("[ConfigManager] Successfully loaded config: " .. configName)
 		return true
 	end
 
