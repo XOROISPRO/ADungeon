582721f31e3186ba05724224771a072bfb305d48
XOROISPRO <xoro4690@gmail.com>
2026-09-01 15:22:29 +0800

    Update DungeonFarmModule.lua

--- DungeonFarmModule.lua
+++ DungeonFarmModule.lua
@@ -4,23 +4,43 @@ DungeonFarmModule.__index = DungeonFarmModule
 
 function DungeonFarmModule.Init(State: any, Toggles: any, PathfindingModule: any)
 	local self = setmetatable({}, DungeonFarmModule)
+
 	self.State = State
 	self.Toggles = Toggles
 	self.PathfindingModule = PathfindingModule
+	self.Running = false
+
 	return self
 end
 
 function DungeonFarmModule:Start()
+	if self.Running then return end
+	self.Running = true
 	self.State.AutoFarmActive = true
+
 	if self.PathfindingModule then
 		self.PathfindingModule:StartHoverTargeting()
 	end
 end
 
 function DungeonFarmModule:Stop()
+	self.Running = false
 	self.State.AutoFarmActive = false
+
 	if self.PathfindingModule then
 		self.PathfindingModule:StopPathfinding()
+	end
+end
+
+function DungeonFarmModule:SetDistance(height: number)
+	if self.PathfindingModule then
+		self.PathfindingModule:SetHoverHeight(height)
+	end
+end
+
+function DungeonFarmModule:SetSpeed(speed: number)
+	if self.PathfindingModule then
+		self.PathfindingModule:SetWalkSpeed(speed)
 	end
 end
 
